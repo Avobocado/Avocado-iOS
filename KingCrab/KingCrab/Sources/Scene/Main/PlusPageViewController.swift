f4d6de85a564626d7a78eae5bc19a2fe3aa1ab8a
john03062 <john03062@dsm.hs.kr>
2024-08-06 21:41:18 +0900

    정리

---
+++ KingCrab/KingCrab/Sources/Scene/Main/PlusPageViewController.swift
@@ -0,0 +1,17 @@
+import Then
+import SnapKit
+import UIKit
+
+class PlusPageViewController: UIViewController {
+
+    let customview = UIView().then {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+
+}
---
+++ KingCrab/KingCrab/Sources/Scene/Main/TableviewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableviewTableViewCell.swift
+//  KingCrab
+//
+//  Created by 허영재 on 7/29/24.
+//
+
+import UIKit
+
+class TableviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
